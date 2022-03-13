4c71bc351bde9c48c7ae5ce6c529238d0eb2ce33
ilhamdwikiarifani <ilhamdwiki0408@gmail.com>
2022-03-13 11:12:22 +0700

    portofolio--kampusmerdeka

---
+++ js/app.js
@@ -0,0 +1,53 @@
+document.querySelector("#gallery1").addEventListener("click", function () {
+  Swal.fire({
+    imageUrl: "../images/srct-2.jpg",
+    imageWidth: 700,
+    imageHeight: 350,
+    imageAlt: "Custom image",
+  });
+});
+
+document.querySelector("#gallery2").addEventListener("click", function () {
+  Swal.fire({
+    imageUrl: "../images/hero--2.jpeg",
+    imageWidth: 300,
+    imageHeight: 500,
+    imageAlt: "Custom image",
+  });
+});
+
+document.querySelector("#gallery3").addEventListener("click", function () {
+  Swal.fire({
+    imageUrl: "../images/hero--3.jpeg",
+    imageWidth: 300,
+    imageHeight: 500,
+    imageAlt: "Custom image",
+  });
+});
+
+document.querySelector("#gallery4").addEventListener("click", function () {
+  Swal.fire({
+    imageUrl: "../images/srct-1.jpg",
+    imageWidth: 700,
+    imageHeight: 350,
+    imageAlt: "Custom image",
+  });
+});
+
+document.querySelector("#gallery5").addEventListener("click", function () {
+  Swal.fire({
+    imageUrl: "../images/hero--1.jpeg",
+    imageWidth: 700,
+    imageHeight: 350,
+    imageAlt: "Custom image",
+  });
+});
+
+document.querySelector("#gallery6").addEventListener("click", function () {
+  Swal.fire({
+    imageUrl: "../images/hero--4.jpeg",
+    imageWidth: 300,
+    imageHeight: 500,
+    imageAlt: "Custom image",
+  });
+});
